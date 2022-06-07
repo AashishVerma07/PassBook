58a2b6fa95197083cff31ab5dae251bba81bd6ef
bhavesh-sp18 <bhavesh.kadam@sparkeighteen.com>
2022-06-07 18:48:30 +0530

    create passbook project

---
+++ src/test/java/sparkeighteen/passbook/PassbookApplicationTests.java
@@ -0,0 +1,13 @@
+package sparkeighteen.passbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
